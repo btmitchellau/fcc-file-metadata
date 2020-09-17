b03be8147e6958047c2bfc054aa2138846fb2e5f
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-17 00:44:05 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req,res) {
+    res.json({"filesize" : req.file.size})
+});
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
