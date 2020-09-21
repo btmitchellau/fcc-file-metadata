c4c4c8c63799efc65aa690164ee8cd9f9c3d3ab9
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-21 15:11:28 +0930

    eslint cleanup etc

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-'use strict';
-
 const express = require('express');
 const cors = require('cors');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.post('/api/fileanalyse', upload.single('upfile'), function (req,res) {
-    res.json({"filesize" : req.file.size})
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({ filesize: req.file.size });
 });
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
+
+app.listen(process.env.PORT || 3000);
